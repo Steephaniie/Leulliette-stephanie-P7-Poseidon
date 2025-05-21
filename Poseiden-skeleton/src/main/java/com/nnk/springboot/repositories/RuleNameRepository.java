0786ad675da3070d85c459c06fcb8094f63ef160
Stéphanie Leulliette <stef_leulliette@yahoo.fr>
2025-05-21 08:32:47 +0200

    "Ajout de javadoc pour les repositories RuleName, Trade et User afin de documenter leur rôle et leurs fonctionnalités principales."

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RuleNameRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RuleNameRepository.java
@@ -4,6 +4,13 @@ import com.nnk.springboot.domain.RuleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository pour la gestion des règles métier dans l'application.
+ * Fournit les opérations CRUD standard pour l'entité RuleName via l'extension de JpaRepository.
+ *
+ * @see RuleName
+ * @see JpaRepository
+ */
 @Repository
 public interface RuleNameRepository extends JpaRepository<RuleName, Integer> {
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
@@ -7,8 +7,23 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Repository pour la gestion des utilisateurs
+ * Fournit les opérations CRUD standard via l'extension de JpaRepository
+ * ainsi que des fonctionnalités de recherche avancées via JpaSpecificationExecutor
+ *
+ * @see User
+ * @see JpaRepository
+ * @see JpaSpecificationExecutor
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
 
+    /**
+     * Recherche un utilisateur par son nom d'utilisateur
+     *
+     * @param username le nom d'utilisateur à rechercher
+     * @return Optional contenant l'utilisateur s'il existe, vide sinon
+     */
     Optional<User> findByUsername(String username);
 }
